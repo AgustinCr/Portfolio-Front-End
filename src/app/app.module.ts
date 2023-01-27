2dc110620cc11272b9189a3ad634dc2a04648f2d
Agustin-Cruz <agustinalexiscruz0905@gmail.com>
2023-01-26 21:35:35 -0300

    primer commit con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SeccionPrincipalComponent } from './componentes/seccion-principal/seccion-principal.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ListaArticulosComponent } from './componentes/lista-articulos/lista-articulos.component';
+import { TestimoniosComponent } from './componentes/testimonios/testimonios.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ComponenteLoginComponent } from './componentes/componente-login/componente-login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    SeccionPrincipalComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    ListaArticulosComponent,
+    TestimoniosComponent,
+    ContactoComponent,
+    FooterComponent,
+    ComponenteLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
